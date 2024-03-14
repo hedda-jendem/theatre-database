9dbb8db32ccfcae40868822f5d8d2895ecff09f9
Hedda Jendem <heddajen@stud.ntnu.no>
2024-03-14 09:13:02 +0100

    Lagde funksjon som leser SLQ fil

--- SetUpDatabase.py
+++ SetUpDatabase.py
@@ -1,44 +1,65 @@
 import sqlite3
 
+def read_SQL(sqlFile):
+    con = sqlite3.connect("TeaterDatabase.db")
+    cursor = con.cursor()
 
-#Setter opp database strukturen 
-con = sqlite3.connect("TeaterDatabase.db")
-cursor = con.cursor()
+    with open('TeaterTabellSetup.sql', 'r') as f:
+        sqlFile = f.read()
 
-with open('TeaterTabellSetup.sql', 'r') as f:
-    sqlFile = f.read()
+    sql_commands = sqlFile.split(';')
 
-sql_commands = sqlFile.split(';')
+    # Execute every command from the input file
+    for command in sql_commands:
+        try:
+            cursor.execute(command)
+        except Exception as e:
+            print('Command skipped: ', e)
 
-# Execute every command from the input file
-for command in sql_commands:
-    try:
-        cursor.execute(command)
-    except Exception as e:
-        print('Command skipped: ', e)
+    con.commit()
+    con.close()
 
-con.commit()
-con.close()
+read_SQL('TeaterTabellSetup.sql')
+read_SQL('DatabaseValues.sql')
+
+# #Setter opp database strukturen 
+# con = sqlite3.connect("TeaterDatabase.db")
+# cursor = con.cursor()
+
+# with open('TeaterTabellSetup.sql', 'r') as f:
+#     sqlFile = f.read()
+
+# sql_commands = sqlFile.split(';')
+
+# # Execute every command from the input file
+# for command in sql_commands:
+#     try:
+#         cursor.execute(command)
+#     except Exception as e:
+#         print('Command skipped: ', e)
+
+# con.commit()
+# con.close()
 
 
-#Setter inn verdier til databasen 
-con = sqlite3.connect("TeaterDatabase.db")
-cursor = con.cursor()
+# #Setter inn verdier til databasen 
+# con = sqlite3.connect("TeaterDatabase.db")
+# cursor = con.cursor()
 
-with open('DatabaseValues.sql', 'r') as f:
-    sqlFile = f.read()
+# with open('DatabaseValues.sql', 'r') as f:
+#     sqlFile = f.read()
 
-sql_commands = sqlFile.split(';')
+# sql_commands = sqlFile.split(';')
 
-# Execute every command from the input file
-for command in sql_commands:
-    try:
-        cursor.execute(command)
-    except Exception as e:
-        print('Command skipped: ', e)
+# # Execute every command from the input file
+# for command in sql_commands:
+#     try:
+#         cursor.execute(command)
+#     except Exception as e:
+#         print('Command skipped: ', e)
 
-con.commit()
-con.close()
+# con.commit()
+# con.close()
 
 
 
