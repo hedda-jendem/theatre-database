9ed435171cf7e8d182bd1f1b2bfd0bf30115e907
Hedda Jendem <heddajen@stud.ntnu.no>
2024-03-15 12:29:32 +0100

    Rydda litt i SetUpDatabase

--- SetUpDatabase.py
+++ SetUpDatabase.py
@@ -22,45 +22,6 @@ def read_SQL(sqlFile):
 read_SQL('TeaterTabellSetup.sql')
 read_SQL('DatabaseValues.sql')
 
-# #Setter opp database strukturen 
-# con = sqlite3.connect("TeaterDatabase.db")
-# cursor = con.cursor()
-
-# with open('TeaterTabellSetup.sql', 'r') as f:
-#     sqlFile = f.read()
-
-# sql_commands = sqlFile.split(';')
-
-# # Execute every command from the input file
-# for command in sql_commands:
-#     try:
-#         cursor.execute(command)
-#     except Exception as e:
-#         print('Command skipped: ', e)
-
-# con.commit()
-# con.close()
-
-
-# #Setter inn verdier til databasen 
-# con = sqlite3.connect("TeaterDatabase.db")
-# cursor = con.cursor()
-
-# with open('DatabaseValues.sql', 'r') as f:
-#     sqlFile = f.read()
-
-# sql_commands = sqlFile.split(';')
-
-# # Execute every command from the input file
-# for command in sql_commands:
-#     try:
-#         cursor.execute(command)
-#     except Exception as e:
-#         print('Command skipped: ', e)
-
-# con.commit()
-# con.close()
-
 
 
 #Setter opp verdier for stoler 
