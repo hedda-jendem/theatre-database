e78282b9ef191c4b07161a9d404cc77057e2ce83
Hedda Jendem <heddajen@stud.ntnu.no>
2024-03-19 09:37:19 +0100

    Kommet till på 3

--- brukstilfelle_3.py
+++ brukstilfelle_3.py
@@ -18,7 +18,16 @@ def ledige_stoler():
     con = sqlite3.connect("TeaterDatabase.db")
     cursor = con.cursor()
 
-    cursor.execute("SELECT Stolrad FROM Stol WHERE Stolnummer,Stolrad, Stolomrade NOT IN (SELECT Stolnummer,Stolrad,Stolomrade, FROM Billett WHERE (ForestillingDato = ?))", (ForestillingDato))
+    cursor.execute("SELECT Radnummer, Omradenavn, SalID "
+               "FROM Stol "
+               "WHERE SalID = (SELECT TeaterSal FROM Stykke WHERE Navn = 'Størst av alt er kjærligheten') "
+               "AND (Stolnummer, Radnummer, Omradenavn) NOT IN "
+               "(SELECT Stolnummer, Stolrad, Stolomrade "
+               "FROM Billett "
+               "WHERE ForestillingDato = '2024-02-03') "
+               "GROUP BY Radnummer, Omradenavn, SalID "
+               "HAVING COUNT(*) > 9 "
+               "ORDER BY Omradenavn;")
 
     con.commit()
     con.close()
