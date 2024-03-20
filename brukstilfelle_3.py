f983fd502a4692d79634748d4373184df749cbb5
Hedda Jendem <heddajen@stud.ntnu.no>
2024-03-20 15:56:28 +0100

    3 som ser ut til å fungere

--- brukstilfelle_3.py
+++ brukstilfelle_3.py
@@ -1,105 +1,122 @@
 import sqlite3
 
-def kjop_billetter():   
-
+def kjop_billetter(BillettType, antallBilletter, ForestillingDato, StykkeNavn, KundeID):   
     con = sqlite3.connect("TeaterDatabase.db")
     cursor = con.cursor()
     
-    # Finner StykkeID for "Størst av alt er kjærligheten"
-    cursor.execute("SELECT StykkeID FROM Stykke WHERE Navn = 'Størst av alt er kjærligheten'")
+    # Finner StykkeID basert på navnet på stykket
+    cursor.execute("SELECT StykkeID FROM Stykke WHERE Navn = ?", (StykkeNavn,))
     StykkeID = cursor.fetchone()[0]
-    ForestillingDato = '2024-02-03'
-    BillettType = 'Voksen'
-    cursor.execute("SELECT Pris FROM Billett WHERE BillettType = 'Voksen'")
+
+    # Finner pris for billettType
+    cursor.execute("SELECT Pris FROM Prisliste WHERE Type = ? AND Stykke = ?", (BillettType, StykkeID))
     Pris = cursor.fetchone()[0]
 
-    # Setter inn ny kunde og henter KundeID
-    cursor.execute('INSERT INTO Kunde (KundeID, Navn) VALUES (?,?)', (None, "Sensorbruker"))
-    KundeNr = cursor.lastrowid
+    # Setter inn ny ordre og henter OrdreID
+    OrdreID = hent_neste_id("Ordre", "OrdreID", con)
+    cursor.execute('INSERT INTO Ordre (OrdreID, KundeID) VALUES (?,?)', (OrdreID, KundeID)) 
 
-    # Setter inn ny ordre
-    cursor.execute('INSERT INTO Ordre (OrdreID, KundeID) VALUES (?,?)', (None, KundeNr)) 
+    # Henter liste over ledige stoler
+    ledigeStoler = ledige_stoler(cursor, ForestillingDato, StykkeID)
 
-    ledigRad = ledige_rad()
-    antallLedigeStoler = ledigRad[3]
+    # Velger ledige stoler på samme rad
+    stolerRad = []
+    for stol in ledigeStoler:
+        if len(stolerRad) == 0 or (stolerRad[-1][1] == stol[1] and stolerRad[-1][2] == stol[2]):
+            stolerRad.append(stol)
+        else:
+            stolerRad = [stol]
 
-    antall_kjopte = 0
-    Stoler = []
-    
-    TotalPris = 0
-
-    for i in range(1, antallLedigeStoler+1):
-        if antall_kjopte == 9:
+        if len(stolerRad) == antallBilletter:
             break
-        if (i, ledigRad[0], ledigRad[1]) not in cursor.execute('SELECT Stolnummer, Stolrad, Stolomrade FROM Billett WHERE ForestillingDato = ?', (ForestillingDato,)).fetchall():
-            print(f"Sete {i} i rad {ledigRad[0]} i området {ledigRad[1]} er tatt")
 
-            Stol = (i, ledigRad[0], ledigRad[1], StykkeID, Pris, BillettType)
-            Stoler.append(Stol)
-            
-            cursor.execute('INSERT INTO Billett (BillettID, OrdreID, StykkeID, Pris, BillettType, Stolnummer, Stolrad, Stolomrade, SalID, ForestillingDato) VALUES (?,?,?,?,?,?,?,?,?,?)', (i+50, 2, StykkeID, Pris, BillettType, i, ledigRad[0], ledigRad[1], 2, ForestillingDato))
-            cursor.execute('INSERT INTO Reservert (Stolnummer, Stolrad, Stolomrade, Sal, Billett, Ordre, ForestillingDato, Stykke, Kunde) VALUES (?,?,?,?,?,?,?,?,?)', (i, ledigRad[0], ledigRad[1], 2, i+50, 2, ForestillingDato, StykkeID, 2))
-            antall_kjopte += 1
-            TotalPris += Pris
-
-    con.commit()
-    con.close()
-
-    print(f"Du har kjøpt {antall_kjopte} billetter til forestillingen Størst av alt er kjærligheten den 3. februar 2024, kostende {TotalPris} kr.")
-    print(f"Du har kjøpt setene {Stoler}")
-
-
-def ledige_rad(): 
-    con = sqlite3.connect("TeaterDatabase.db")
-    cursor = con.cursor()
-
-    cursor.execute('''
-            SELECT Radnummer, Omradenavn, SalID, COUNT(*)
-            FROM Stol 
-            WHERE SalID = (SELECT TeaterSal FROM Stykke WHERE Navn = 'Størst av alt er kjærligheten') 
-            AND (Stolnummer, Radnummer, Omradenavn) NOT IN 
-            (SELECT Stolnummer, Stolrad, Stolomrade 
-            FROM Billett 
-            WHERE ForestillingDato = '2024-02-03') 
-            GROUP BY Radnummer, Omradenavn, SalID 
-            ORDER BY Omradenavn;
-            ''')
-
-    resultater = cursor.fetchall()
-    print(resultater[0])
+    if len(stolerRad) == antallBilletter:
+        for stol in stolerRad:
+            BillettID = hent_neste_id("Billett", "BillettID", con)
+            cursor.execute('INSERT INTO Billett (BillettID, OrdreID, StykkeID, Pris, BillettType, Stolnummer, Stolrad, Stolomrade, SalID, ForestillingDato) VALUES (?,?,?,?,?,?,?,?,?,?)', 
+                           (BillettID, OrdreID, StykkeID, Pris, BillettType, stol[0], stol[1], stol[2], 2, ForestillingDato))
+            cursor.execute('INSERT INTO Reservert (Stolnummer, Stolrad, Stolomrade, Sal, Billett, Ordre, ForestillingDato, Stykke, Kunde) VALUES (?,?,?,?,?,?,?,?,?)', 
+                           (stol[0], stol[1], stol[2], 2, BillettID, OrdreID, ForestillingDato, StykkeID, KundeID))
+    else:
+        print("Ikke nok ledige stoler tilgjengelig.")
 
     con.commit()
     con.close()
 
-    return resultater[0] #Velger ut den første raden med 9 ledige stoler
+    print(stolerRad)
 
 
-def ledige_stoler():
-    con = sqlite3.connect("TeaterDatabase.db")
-    cursor = con.cursor()
+def ledige_stoler(cursor, ForestillingDato, StykkeID):
+    cursor.execute("""
+        SELECT Stolnummer, Radnummer, Omradenavn
+        FROM Stol 
+        WHERE SalID = (SELECT TeaterSal FROM Stykke WHERE StykkeID = ?) 
+        AND (Stolnummer, Radnummer, Omradenavn) NOT IN 
+        (SELECT Stolnummer, Stolrad, Stolomrade 
+        FROM Billett 
+        WHERE ForestillingDato = ?)""",
+        (StykkeID, ForestillingDato))
+    return cursor.fetchall()
 
-    cursor.execute('''
-            SELECT Stolnummer, Radnummer, Omradenavn
-            FROM Stol 
-            WHERE SalID = (SELECT TeaterSal FROM Stykke WHERE Navn = 'Størst av alt er kjærligheten') 
-            AND (Stolnummer, Radnummer, Omradenavn) NOT IN 
-            (SELECT Stolnummer, Stolrad, Stolomrade 
-            FROM Billett 
-            WHERE ForestillingDato = '2024-02-03');
-            ''')
+# def ledige_stoler(ForestillingDato, StykkeID):
+#     con = sqlite3.connect("TeaterDatabase.db")
+#     cursor = con.cursor()
 
-    resultater = cursor.fetchall()
+#     cursor.execute('''
+#         SELECT Stolnummer, Radnummer, Omradenavn
+#         FROM Stol 
+#         WHERE SalID = (SELECT TeaterSal FROM Stykke WHERE StykkeID = ?) 
+#         AND (Stolnummer, Radnummer, Omradenavn) NOT IN 
+#         (SELECT Stolnummer, Stolrad, Stolomrade 
+#         FROM Billett 
+#         WHERE ForestillingDato = ?)''',
+#         (StykkeID, ForestillingDato))
+    
+#     resultater = cursor.fetchall()
+#     con.commit()
+#     con.close()
 
-    con.commit()
-    con.close()
+#     return resultater
 
 
+
+# def hentBilletID():
+#     con = sqlite3.connect("TeaterDatabase.db")
+#     cursor = con.cursor()
+
+#     cursor.execute('''
+#             SELECT MAX(BillettID)
+#             FROM Billett;
+#             ''')
+
+#     resultater = cursor.fetchall()
+
+#     con.commit()
+#     con.close()
+
+#     return resultater[0][0] +1
     
 
+def hent_neste_id(tabell, kolonne, con):
+    cursor = con.cursor()
+    cursor.execute(f"SELECT MAX({kolonne})+1 FROM {tabell}")
+    maks_id = cursor.fetchone()[0]
+    return maks_id if maks_id is not None else 1
 
-ledige_rad()
-ledige_stoler()
-kjop_billetter()    
+# ledige_rad()
+#ledige_stoler()
+
+
+con = sqlite3.connect("TeaterDatabase.db")
+cursor = con.cursor()
+
+cursor.execute('INSERT INTO Kunde (KundeID, Navn) VALUES (?,?)', (2, "Sensorbruker"))
+
+con.commit()
+con.close()
+
+kjop_billetter('Ordinær', 9, '2024-02-03', 'Størst av alt er kjærligheten', 2)
+#kjop_billetter('Voksen', 9, '2024-02-03', 2, 2)    
 
 
 
