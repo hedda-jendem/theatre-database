ef093452843bf301b9113147b496db24b9132127
Thomas Haugli <thomas.djonne.haugli@gmail.com>
2024-03-20 14:01:32 +0100

    mer arbeid på brukerhistorie 7

--- brukstilfelle_7.py
+++ brukstilfelle_7.py
@@ -6,23 +6,32 @@ def finnSkuespillerISammeAkt(skuespiller):
 
     resultat = []
 
-    #SQL spørring som finner alle aktene den skuespilleren er med i
-    cursor.execute("""SELECT Akt FROM RolleIAkt WHERE Rolle IN 
-                   (SELECT Rolle FROM Skuespiller WHERE Navn = ?)""",(skuespiller))
+    #SQL spørring som finner alle aktene en skuespiller er med i
+    cursor.execute("""SELECT Akt, StykkeID FROM RolleIAkt 
+                   WHERE Rolle = (SELECT Rolle FROM RolleBesetning 
+                   WHERE Skuespiller = (SELECT SkuespillerID FROM Skuespiller 
+                   WHERE Navn = ?))""", (skuespiller,))
     listeMedAkter = cursor.fetchall()
-    
+
     #SQL spørring som finner alle skuespillere som er med i de samme aktene 
     for akt in listeMedAkter:
-        cursor.execute("""SELECT DISTINCT Skuespiller.Navn FROM Skuepiller 
-                       JOIN RolleIAkt ON Skuespiller.SkuespillerID = RolleIAkt.Rolle
-                       WHERE RolleIAkt = ?""", (akt))
-    listeOverSkuespillereIAktene = cursor.fetchall()
+        AktNr = akt[0]
+        SkuespillID = akt[1]
+        cursor.execute("""SELECT DISTINCT Skuespiller.Navn, Stykke.Navn FROM RolleIAkt 
+                       JOIN Skuespiller ON RolleIAkt.Rolle = Skuespiller.SkuespillerID
+                       JOIN Stykke ON RolleIAkt.StykkeID = Stykke.StykkeID
+                       WHERE RolleIAkt.Akt = ? AND Skuespiller.Navn != ? AND RolleIAkt.StykkeID = ?
+                       """, (AktNr, skuespiller, SkuespillID,))
+        listeOverSkuespillereMedIAkt = cursor.fetchall()
+        resultat.append(listeOverSkuespillereMedIAkt)
 
-    print(listeMedAkter)
-    print(listeOverSkuespillereIAktene)
     con.close()
 
+    print("")
+    print(f'Her kommer alle skuespillere som har spilt med {skuespiller}, og hvilket skuespill det skjedde i')
+    for lineEN in resultat:
+        for lineTO in lineEN:
+            print(f'{skuespiller} spilte med {lineTO[0]} i {lineTO[1]}')
 
 
-    #Håkon Håkonsson
     #Arturo Scotti
